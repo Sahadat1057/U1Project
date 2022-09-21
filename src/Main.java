a65cc1259f175ade38175b9e607d41b2a13efdb0
BT_4N2_11 <samzad7698@bths.edu>
2022-09-21 12:28:48 -0400

    Added some variables and scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("U1Project");
+        System.out.println("Tip Calculator");
+        Scanner s = new Scanner(System.in);
+        System.out.println("How much does the meal cost? ");
+        double mealAmount = s.nextDouble;
+        System.out.println("How much is the tip");
+
+        double tip = s.nextDouble;
+        double tipPercentage = tip / 100;
+
+
+
+
     }
 }
