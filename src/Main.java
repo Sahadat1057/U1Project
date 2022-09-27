a4a80be1f98dd532336d622e0e02a2b971f6b19c
sahadatamzad <samzad7698@bths.edu>
2022-09-27 18:44:45 -0400

    I have changed up the format a bit. Included the initial print statements together. Then I included the input variables with a bit of print statements together. Lastly, I included the print statements with string concatenation together.

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Tip Calculator:");
-        System.out.print("Enter the cost of the meal: $");
-        double mealAmount = s.nextDouble();
-        System.out.print("Select how much you would like to tip: ");
-        double tip = s.nextDouble();
-        System.out.print("Select how many people are there: ");
-        int numberOfPeople = s.nextInt();
 
-        double tipPercentage = tip / 100;
-        double totalTip = Math.round((mealAmount * tipPercentage) * 100.0) / 100.0;
-        double tipPerPerson = Math.round((totalTip / numberOfPeople) * 100.0) / 100.0;
-        double totalBill = Math.round((mealAmount + totalTip) * 100.0) / 100.0;
-        double totalPerPerson = Math.round((totalBill / numberOfPeople) * 100.0) / 100.0;
-        System.out.println("The total tip amount is: $" + totalTip);
-        System.out.println("The individual tip amount is: $" + tipPerPerson);
-        System.out.println("The total bill is: $" + totalBill);
-        System.out.println("The individual total per person is: $" + totalPerPerson);
+            System.out.println("Tip Calculator:");
+            System.out.print("Enter the cost of the meal: $");
+
+            double mealAmount = s.nextDouble();
+            System.out.print("Select how much you would like to tip: ");
+            double tip = s.nextDouble();
+            System.out.print("Select how many people are there: ");
+            int numberOfPeople = s.nextInt();
+            double tipPercentage = tip / 100;
+            double totalTip = Math.round((mealAmount * tipPercentage) * 100.0) / 100.0;
+            double tipPerPerson = Math.round((totalTip / numberOfPeople) * 100.0) / 100.0;
+            double totalBill = mealAmount + totalTip;
+            double totalPerPerson = Math.round((totalBill / numberOfPeople) * 100.0) / 100.0;
+
+
+            System.out.println("The total tip amount is: $" + totalTip);
+            System.out.println("The individual tip amount is: $" + tipPerPerson);
+            System.out.println("The total bill is: $" + totalBill);
+            System.out.println("The individual total per person is: $" + totalPerPerson);
     }
 }
