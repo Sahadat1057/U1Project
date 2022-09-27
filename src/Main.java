310aa466777de58c0e2803635f0ec6119feb7785
sahadatamzad <samzad7698@bths.edu>
2022-09-27 18:37:19 -0400

    I have added all my variables with rounding and expressions plus print statements

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tip Calculator");
         Scanner s = new Scanner(System.in);
-        System.out.println("How much does the meal cost? ");
-        double mealAmount = s.nextDouble;
-        System.out.println("How much is the tip");
+        System.out.println("Tip Calculator:");
+        System.out.print("Enter the cost of the meal: $");
+        double mealAmount = s.nextDouble();
+        System.out.print("Select how much you would like to tip: ");
+        double tip = s.nextDouble();
+        System.out.print("Select how many people are there: ");
+        int numberOfPeople = s.nextInt();
 
-        double tip = s.nextDouble;
         double tipPercentage = tip / 100;
-
-
-
-
+        double totalTip = Math.round((mealAmount * tipPercentage) * 100.0) / 100.0;
+        double tipPerPerson = Math.round((totalTip / numberOfPeople) * 100.0) / 100.0;
+        double totalBill = Math.round((mealAmount + totalTip) * 100.0) / 100.0;
+        double totalPerPerson = Math.round((totalBill / numberOfPeople) * 100.0) / 100.0;
+        System.out.println("The total tip amount is: $" + totalTip);
+        System.out.println("The individual tip amount is: $" + tipPerPerson);
+        System.out.println("The total bill is: $" + totalBill);
+        System.out.println("The individual total per person is: $" + totalPerPerson);
     }
 }
